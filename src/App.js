ce04921b1efe77b90607e502d414964dc33a4080
anandhi2017 <anandhi@systimanx.com>
2022-06-12 20:05:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
 
+import "./App.css";
+import Home from "../src/pages/Home";
+import About from "../src/pages/About";
+import Contact from "../src/pages/Contact";
+ import Header from "./components/Header";
+ import Footer from "./components/Footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>   
+     <Header /> 
+      <Routes>
+        <Route exact  path="/" element={<Home />} />
+        {/* <Route  exact path="/about" element={<About />} /> */}
+        <Route exact path='/about' element={<About />}></Route> 
+        <Route exact  path="/contact" element={<Contact />} > </Route> 
+      </Routes>
+       <Footer />      
+      </>   
   );
 }
 
